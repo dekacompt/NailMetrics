8b97e32f1d0d3a96be63d4aed8efa09587bbfef8
Deka <dekacompt@gmail.com>
2026-01-17 00:46:54 +0800

    fix: fix 404

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/finger/', // Fixes 404s on GitHub Pages (repo suffix)
+  base: './', // Use relative paths (safer for any folder depth)
 })
